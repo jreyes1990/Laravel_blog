590977103928bd6965bcd3945fca4b452a8bbefa
Jolberth Reyes <jarl1990mt@gmail.com>
2022-11-15 23:09:03 -0600

    Tinker: Leer datos y usar restricciones

--- app/Models/Ejemplo.php
+++ app/Models/Ejemplo.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Ejemplo extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    //use SoftDeletes;
 
     //protected $table = 'ejemplo';
     protected $table = 'tinker';
